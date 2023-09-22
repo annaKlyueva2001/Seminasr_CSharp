e461d558f986ad63d32177431dc0e5f23f7e92e7
Anna <anna.klyueva2001@gmail.com>
2023-09-22 23:26:22 +0300

    Третья задача ДЗ1

--- Homework/Homeworc001/Program.cs
+++ Homework/Homeworc001/Program.cs
@@ -30,29 +30,40 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
-Console.WriteLine("Input first number: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input third number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-
-int max = num1;
-if(max < num2)
-{
-    max = num2;
-}
-if(max < num3)
-{
-    max = num3;
-}
-Console.WriteLine($"Maximum number is {max}");
+//Console.WriteLine("Input first number: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input second number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input third number: ");
+//int num3 = Convert.ToInt32(Console.ReadLine());
+//
+//int max = num1;
+//if(max < num2)
+//{
+//    max = num2;
+//}
+//if(max < num3)
+//{
+//    max = num3;
+//}
+//Console.WriteLine($"Maximum number is {max}");
 
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 //5 -> 2, 4

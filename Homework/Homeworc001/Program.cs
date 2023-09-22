0279db4d0088356d0dcb53f010fd10e1ad6523b7
Anna <anna.klyueva2001@gmail.com>
2023-09-22 23:50:37 +0300

    Немного исправили для красоты

--- Homework/Homeworc001/Program.cs
+++ Homework/Homeworc001/Program.cs
@@ -38,14 +38,9 @@
 //int num3 = Convert.ToInt32(Console.ReadLine());
 //
 //int max = num1;
-//if(max < num2)
-//{
-//    max = num2;
-//}
-//if(max < num3)
-//{
-//    max = num3;
-//}
+//if(max < num2) max = num2;
+//if(max < num3) max = num3;
+
 //Console.WriteLine($"Maximum number is {max}");
 
 

01a80d5bc72283274e1c7ba47088038c2931937c
Anna <anna.klyueva2001@gmail.com>
2023-09-23 22:24:58 +0300

    Лекция по методам и массивам

--- Lecture/Lecture02/Program.cs
+++ Lecture/Lecture02/Program.cs
@@ -17,17 +17,70 @@
 //Console.WriteLine(max);
 
 
-int [] array = { 1, 21, 31, 4, 15, 16, 17, 18};
+//int [] array = { 1, 21, 31, 4, 18, 15, 16, 17, 18};
 //array[0] = 12; //поменять значение внутри массива
 //Console.WriteLine(array[4]); // вытащить число с индексом 0
-int n = array[^1];
-int find = 4;
-int index = 0;
-while(index < n)
+//int n = array.Length;
+//int find = 18;
+//int index = 0;
+//while(index < n)
+//{
+//    if(array[index] == find)
+//    {
+//        Console.WriteLine(index);
+//        break;  //выходи из цикла как только нашел
+//    }
+//    index++;
+//}
+
+
+
+// Функция и метод одно и то же
+
+void FillArray(int[] collection)  //метод, который использует переменную-массив collection
 {
-    if(array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while ( index < length)
     {
-        Console.WriteLine(index);
+        collection[index] = new Random().Next(1, 10);
+        index++;
     }
-    index++;
 }
+
+void PrintArray(int[] col)  // void используется, если переменную не надо возвращать в программу
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while ( index < count )
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //создай новый массив из 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos); 
